e26ef47ad12519a301ee00fa1a3f601715060c42
Jake <jake.gozdziewski@gmail.com>
2022-11-10 13:58:38 +0100

    UX: Add UserHeader component for the top app bar

--- app/src/main/java/com/muzz/chatty/ui/chat/AppBarComponents.kt
+++ app/src/main/java/com/muzz/chatty/ui/chat/AppBarComponents.kt
@@ -1,23 +1,21 @@
 package com.muzz.chatty.ui.chat
 
-import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.Person3
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
-import com.muzz.chatty.ui.theme.BasicPreview
-import com.muzz.chatty.ui.theme.ChattyTheme
-import com.muzz.chatty.ui.theme.MuzzPink
-import com.muzz.chatty.ui.theme.MuzzYellow
+import com.muzz.chatty.ui.theme.*
 
 /** Back button for the top app bar */
 @OptIn(ExperimentalMaterialApi::class)
@@ -57,6 +55,51 @@ internal fun BackButtonPreview() {
         BackButton(
             onClick = {  },
             modifier = Modifier.requiredSize(56.dp)
+        )
+    }
+}
+
+
+
+/** Header for the user [avatar] and [name]. Using icon instead of picture for simplicity */
+@Composable
+fun UserHeader(
+    name: String,
+    avatar: ImageVector,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Surface(
+            color = MuzzGray,
+            shape = CircleShape,
+            modifier = Modifier.size(30.dp)
+        ) {
+            Icon(
+                imageVector = avatar,
+                contentDescription = null,
+                tint = MuzzPink,
+                modifier = Modifier.padding(2.dp)
+            )
+        }
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
+
+@BasicPreview
+@Composable
+internal fun UserHeaderPreview() {
+    ChattyTheme {
+        UserHeader(
+            name = "Sarah",
+            avatar = Icons.Default.Person3
         )
     }
 }

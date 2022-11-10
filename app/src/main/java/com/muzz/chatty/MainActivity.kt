cbbb6cc906d37ef83d05915dacf211ab5d2e55db
Jake <jake.gozdziewski@gmail.com>
2022-11-10 14:21:06 +0100

    Feature: Replace existing MainActivity content with MainScreen

--- app/src/main/java/com/muzz/chatty/MainActivity.kt
+++ app/src/main/java/com/muzz/chatty/MainActivity.kt
@@ -3,13 +3,7 @@ package com.muzz.chatty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.muzz.chatty.ui.MainScreen
 import com.muzz.chatty.ui.theme.ChattyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ChattyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ChattyTheme {
-        Greeting("Android")
     }
 }

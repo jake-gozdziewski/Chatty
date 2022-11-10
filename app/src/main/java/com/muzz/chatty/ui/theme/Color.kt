d2c7e65ce1cf987f2fe24cebdadc285a4cd287fa
Jake <jake.gozdziewski@gmail.com>
2022-11-10 11:59:42 +0100

    UX: Set up Compose theming
    Using a simplified version to avoid overcomplicating. Only the colors
    and typography needed for this specific screen.

--- app/src/main/java/com/muzz/chatty/ui/theme/Color.kt
+++ app/src/main/java/com/muzz/chatty/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.muzz.chatty.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val MuzzPink = Color(0xFFFF1278)
+val MuzzSnow = Color(0xFFF2F5FB)
+val MuzzYellow = Color(0xFFFF9F3E)
+val MuzzGray = Color(0xFFC6CCD3)

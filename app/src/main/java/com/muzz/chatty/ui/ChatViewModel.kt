4cb4ebc3e5f86ebf7e1f49327972d1ad1e1723f3
Jake <jake.gozdziewski@gmail.com>
2022-11-10 15:33:31 +0100

    Feature: Connect MessageRepository with the UI through ChatViewModel

--- app/src/main/java/com/muzz/chatty/ui/ChatViewModel.kt
+++ app/src/main/java/com/muzz/chatty/ui/ChatViewModel.kt
@@ -1,50 +1,41 @@
 package com.muzz.chatty.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.muzz.chatty.data.MessageRepository
 import com.muzz.chatty.model.ChatMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
-    repository: MessageRepository
+    private val repository: MessageRepository
 ): ViewModel() {
 
-    val messageList: Flow<List<ChatMessage>> = flow {
-        emit(
-            listOf(
-                ChatMessage(
-                    content = "Yeh for sure that works. What time do you think?",
-                    header = "Monday 21:57",
-                    isReply = true,
-                    showTail = false
-                ),
-                ChatMessage(
-                    content = "Does 7pm work for you? I've got to go pick up my little brother first from a party",
-                    isReply = false,
-                    showTail = true
-                ),
-                ChatMessage(
-                    content = "Ok cool!",
-                    header = "Tuesday 12:36",
-                    isReply = true,
-                    showTail = true
-                ),
-                ChatMessage(
-                    content = "What are you up to today?",
-                    isReply = false,
-                    showTail = false
-                )
+    val messageList: Flow<List<ChatMessage>> = repository.getMessages().map { messageList ->
+        messageList.map { message ->
+            ChatMessage(
+                content = message.content,
+                isReply = message.userId != 0,
+                header = "", // TODO: Implement logic
+                showTail = true // TODO: Implement logic
             )
+        }
+    }
+
+    fun sendMessage(text: String, isReply: Boolean) = viewModelScope.launch {
+        repository.addMessage(
+            content = text,
+            userId = if (isReply) 1 else 0
         )
     }
 
-    fun sendMessage(text: String, isReply: Boolean) { }
-
-    fun restoreContent() { }
+    fun restoreContent() = viewModelScope.launch {
+        repository.restoreOriginalContent()
+    }
 
     fun changeUser() { }
 

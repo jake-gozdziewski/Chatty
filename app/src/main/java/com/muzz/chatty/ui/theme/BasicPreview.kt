00ed9dda478cf70a283d22fbe75cc215c95f6486
Jake <jake.gozdziewski@gmail.com>
2022-11-10 12:07:46 +0100

    Model: Add a simple preview class

---
+++ app/src/main/java/com/muzz/chatty/ui/theme/BasicPreview.kt
@@ -0,0 +1,12 @@
+package com.muzz.chatty.ui.theme
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(
+    name = "Light", group = "Light",
+    showBackground = true, backgroundColor = 0xFFFFFFFF,
+    device = Devices.PIXEL_4, showSystemUi = true,
+)
+annotation class BasicPreview

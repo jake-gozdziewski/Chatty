61bee6f991b39f80a3f9cc135a104973b9025b36
Jake <jake.gozdziewski@gmail.com>
2022-11-10 16:16:21 +0100

    Feature: Implement logic for showing tails and headers

--- app/src/main/java/com/muzz/chatty/ui/ChatViewModel.kt
+++ app/src/main/java/com/muzz/chatty/ui/ChatViewModel.kt
@@ -8,6 +8,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,12 +20,16 @@ class ChatViewModel @Inject constructor(
 ): ViewModel() {
 
     val messageList: Flow<List<ChatMessage>> = repository.getMessages().map { messageList ->
-        messageList.map { message ->
+        messageList.mapIndexed { index, message ->
             ChatMessage(
                 content = message.content,
                 isReply = message.userId != 0,
-                header = "", // TODO: Implement logic
-                showTail = true // TODO: Implement logic
+                header = if (
+                    index == 0 || message.timeSent - messageList[index - 1].timeSent > 1.hours
+                ) makeHeader(message.timeSent) else "",
+                showTail = index == messageList.size - 1
+                        || messageList[index + 1].userId != message.userId
+                        || messageList[index + 1].timeSent - message.timeSent > 20.seconds
             )
         }
     }
@@ -38,5 +46,12 @@ class ChatViewModel @Inject constructor(
     }
 
     fun changeUser() { }
+
+    private fun makeHeader(timeSent: Long): String =
+        ZonedDateTime.ofInstant(Instant.ofEpochMilli(timeSent), ZoneId.systemDefault())
+            .format(DateTimeFormatter.ofPattern("EEEE HH:mm"))
+
+    private val Int.hours: Long get() = this.seconds * 3600
+    private val Int.seconds: Long get() = this * 1_000L
 
 }
